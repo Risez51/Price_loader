88bd3b8b8184f770ab68ba3e8098408154924476
Risez51 <squexke@gmail.com>
2022-02-28 12:01:55 +0300

    1. парсер а4

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import parsers.parser_torg_7
 import parsers.parser_kvt_sitomo
+import parsers.parser_a4_id
 import fileWorker
 
 def main():
-    torg_7_result = parsers.parser_torg_7.Parser_torg_7().parse()
-    fileWorker.FileWriter(0, 'торг7_ЧК').to_excel(torg_7_result)
-    kvt_sitomo_result = parsers.parser_kvt_sitomo.Parser_kvt_sitomo().parse()
-    fileWorker.FileWriter(0, 'КВТ_СИТ').to_excel(kvt_sitomo_result)
-    input('Введите любой символ: ')
+    #torg_7_result = parsers.parser_torg_7.Parser_torg_7().parse()
+    #fileWorker.FileWriter(0, 'торг7_ЧК').to_excel(torg_7_result)
+    #kvt_sitomo_result = parsers.parser_kvt_sitomo.Parser_kvt_sitomo().parse()
+    #fileWorker.FileWriter(0, 'КВТ_СИТ').to_excel(kvt_sitomo_result)
+    a4_result = parsers.parser_a4_id.Parser_a4_id().parse()
+    fileWorker.FileWriter(0, 'А4_ИД').to_excel(a4_result)
+
 if __name__ == '__main__':
     main()
